e0144b4b5574911d21f595d01348aaa7faead903
Stuart Swindells <sswindells@geeksoc.org>
2013-10-05 21:38:59 +0100

    Revert "Combination of work" Revert merge since it doesn't appear to work
    This reverts commit f185bddf8406a066bf855b53d0a3c493d9b10c0a.

--- plugin.py
+++ plugin.py
@@ -10,9 +10,8 @@ from supybot.commands import *
 import supybot.plugins as plugins
 import supybot.ircutils as ircutils
 import supybot.callbacks as callbacks
-import supybot.conf as conf
 import time as epoch
-import os, sys, requests
+import os, sys, ldap
 
 
 class GeekSoc(callbacks.Plugin):
@@ -24,69 +23,93 @@ class GeekSoc(callbacks.Plugin):
         super(GeekSoc, self).__init__(irc)
     
     def userinfo(self, irc, msg, args, name):
-		"""<username>
-
-		Return information on a given username.
-		"""
-		user = name
-		day = int(epoch.time()/(60*60*24))
-		api_url = self.registryValue('protocol')+"://"+self.registryValue('user')+":"+self.registryValue('password')+"@"+self.registryValue('url')
-		r=requests.get(api_url+"/users/"+user )
-		entry = r.json()  
-		if 'error' in entry:
-			irc.reply(("Error: "+entry['error']).encode('utf-8'))
-			return
-		name = entry['displayname']
-		email = entry['email']
-		studentNo = entry['studentnumber'] if 'studentnumber' in entry else "N/A"
-		expiry = entry['expiry'] if 'expiry' in entry else "999999"
-		paid = entry['paid'] if 'paid' in entry else "N/A"
-		
-		
-		groups = ""
-		for group in entry['groups']:
-			groups = groups + group +", "
-		
-		groups = groups[:-2]
-    
-		status = "Active"
-		if (paid == False):
-			status = "Active (Not Paid)"
-		# if int(expiry) <= day+60:
-			# status = "Expiring (in %s days)" % (int(expiry)-day)
-		# if int(expiry) <= day:
-			# status = "Expired (%s days ago)" % (day-int(expiry))
-		if entry['isAdmin'] == True:
-			status = "Admin"
+        """<username>
+         
+         Return information on a given username.
+        """
+        user = name
+        day = int(epoch.time()/(60*60*24))
         
-		string = ( "User: %s, Name: %s, Email: %s, Student Number: %s, Status: %s, Expires: %s" % (user, name, email, studentNo, status, expiry) )
-		irc.reply(string.encode('utf-8'))
-		irc.reply(("Groups: "+groups).encode('utf-8'))
+        try:
+            l = ldap.open("ldap.geeksoc.org")
+            l.protocol_version = ldap.VERSION3
+        except ldap.LDAPError, e:
+            irc.reply('Error getting info for user: "%s"' % name)
+            return
+        
+        baseDN = "ou=People, dc=geeksoc, dc=org"
+        searchScope = ldap.SCOPE_SUBTREE
+        retrieveAttributes = None
+        searchFilter = "uid={0}".format(user)
+        
+        try:
+            results = l.search_s(baseDN, searchScope, searchFilter, retrieveAttributes)
+            if (len(results) == 0):
+                irc.reply('User "%s" doesn\'t exist' % name)
+                return
+    
+            for dn, entry in results:
+                name = entry['cn'][0]
+                email = entry['mail'][0]
+                studentNo = entry['studentNumber'][0] if 'studentNumber' in entry else "N/A"
+                expiry = entry['shadowExpire'][0] if 'shadowExpire' in entry else "999999"
+                paid = entry['hasPaid'][0] if 'hasPaid' in entry else "N/A"
+                
+                status = "Active"
+                if (paid == "FALSE"):
+                    status = "Active (Not Paid)"
+                if int(expiry) <= day+60:
+                    status = "Expiring (in %s days)" % (int(expiry)-day)
+                if int(expiry) <= day:
+                    status = "Expired (%s days ago)" % (day-int(expiry))
+                if int(expiry) == 1:
+                    status = "Admin disabled"
+                
+                string = ( "User: %s, Name: %s, Email: %s, Student Number: %s, Status: %s" % (user, name, email, studentNo, status) )
+                irc.reply(string.encode('utf-8'))
+            
+            baseDN = "ou=Groups, dc=geeksoc, dc=org"
+            searchFilter = "(memberUid=*)"
+            results = l.search_s(baseDN, ldap.SCOPE_SUBTREE, searchFilter)
+            groups = ''
+            for dn, entry in results:
+                if user in entry['memberUid']:
+                    groups += entry['cn'][0] + ' '
+            irc.reply("Groups: " + groups)
+        
+        except ldap.LDAPError, e:
+            irc.reply(e)
 
     userinfo = wrap(userinfo, ['text'])
 
 
     def group(self, irc, msg, args, name):
-		"""<group>
-		 
-		 Return information on a given group.
-		"""
-		group = name
-		api_url = self.registryValue('protocol')+"://"+self.registryValue('user')+":"+self.registryValue('password')+"@"+self.registryValue('url')
-		r=requests.get(api_url+"/groups/"+group )
-		entry = r.json()  
-		if 'error' in entry:
-			irc.reply(("Error: "+entry['error']).encode('utf-8'))
-			return
-		name = entry['name']
-		
-		members = ""
-		for member in entry['members']:
-			members = members + member +", "
-		
-		members = members[:-2]
-		
-		irc.reply(("Name: "+name+" Members: "+members).encode('utf-8'))
+        """<group>
+         
+         Return information on a given group.
+        """
+        group = name
+        
+        try:
+            l = ldap.open("ldap.geeksoc.org")
+            l.protocol_version = ldap.VERSION3
+        except ldap.LDAPError, e:
+            irc.reply('Error getting info for user: "%s"' % name)
+            return
+        
+        baseDN = "cn=%s,ou=Groups, dc=geeksoc, dc=org" %(group)
+        searchFilter = "(memberUid=*)"
+        
+        try:
+            results = l.search_s(baseDN, ldap.SCOPE_SUBTREE, searchFilter)
+            string = ''
+            for dn, entry in results:
+                for u in entry['memberUid']:
+                    string += u + ' '
+            irc.reply("Group: %s, Members: %s" % (group, string))
+
+        except ldap.LDAPError, e:
+            irc.reply(e)
 
     group = wrap(group, ['text'])
 
